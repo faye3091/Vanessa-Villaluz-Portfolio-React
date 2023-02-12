b86aa2655d9a90c44fcfa96576fbbe0622d19c23
Vanessa <faye_3091@yahoo.com>
2023-02-11 21:46:36 -0500

    added projects

--- src/components/PortfolioPage.js
+++ src/components/PortfolioPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Project from "./projectsDone";
 
 export default function PortfolioPage() {
     return (
---
+++ src/components/projectsDone.js
@@ -0,0 +1,75 @@
+import movieLover from "../assets/images/movieLover.jpg";
+import expressFitness from "../assets/images/Ex-Press-Fitness.jpg";
+import movieRandomizer from "../assets/images/MovieRandomizzer.png";
+import weatherDashboard from "../assets/images/weather-dashboard.png";
+import quizChallenge from "../assets/images/Quiz-Challenge.png";
+import passwordGenerator from "../assets/images/Password-Generator.png";
+import dayScheduler from "../assets/images/Work-Day-Scheduler.png";
+import FlipACoin from "../assets/images/Flip-a-coin-game.png";
+
+export default [
+    {
+    "id": 1,
+    "name": "Movie_lover",
+    "image": movieLover,
+    "github": "https://github.com/faye3091/Project-3-client",
+    "deploy": "https://movie-lover.herokuapp.com/",
+    "topics": "node.js, express.js, react-bootstrap, graphql, sequelize, bcrypt, MongoDB, Mongoose, Apollo server, Apollo client, webtoken"
+    },
+    {
+    "id": 2,
+    "name": "Ex-Press Fitness",
+    "image": expressFitness,
+    "github": "https://github.com/faye3091/Ex-Press-Fitness",
+    "deploy": "https://floating-beyond-34615.herokuapp.com/",
+    "topics": "express-js, mysql, sequelize, node-js, handlebars-js"
+    },
+    {
+    "id": 3,
+    "name": "Movie Randomizer",
+    "image": movieRandomizer,
+    "github": "https://github.com/faye3091/Movie-Randomizer",
+    "deploy": "https://faye3091.github.io/Movie-Randomizer/",
+    "topics": "HTML, CSS, Javascript"
+    },
+    {
+    "id": 4,
+    "name": "Weather Dashboard",
+    "image": weatherDashboard,
+    "github": "https://github.com/faye3091/06-Server-Side-APIs-Challenge-Weather-Dashboard",
+    "deploy": "https://faye3091.github.io/06-Server-Side-APIs-Challenge-Weather-Dashboard/",
+    "topics": "Javascript, jQuery, HTML"
+    },
+    {
+    "id": 5,
+    "name": "Quiz Challenge",
+    "image": quizChallenge,
+    "github": "https://github.com/faye3091/Web-APIs-Challenge-4-Code-Quiz",
+    "deploy": "https://faye3091.github.io/Web-APIs-Challenge-4-Code-Quiz/",
+    "topics": "HTML, CSS, JavaScript"
+      },
+    {
+    "id": 6,
+    "name": "Work Day Schedular",
+    "image": dayScheduler,
+    "github": "https://github.com/faye3091/05-Third-Party-APIs-Challenge-Work-Day-Scheduler",
+    "deploy": "https://faye3091.github.io/05-Third-Party-APIs-Challenge-Work-Day-Scheduler/",
+    "topics": "HTML, Javascript, CSS, jQuery, Moment"
+    },
+    {
+    "id": 7,
+    "name": "Password Generator",
+    "image": passwordGenerator,
+    "github": "https://github.com/faye3091/03-JavaScript-Password-Generator",
+    "deploy": "https://faye3091.github.io/03-JavaScript-Password-Generator/",
+    "topics": "HTML, Javascript, CSS"
+    },
+    {
+    "id": 8,
+    "name": "Flip A Coin",
+    "image": FlipACoin,
+    "github": "https://github.com/faye3091/Implement-Coin-Flipper",
+    "deploy": "https://faye3091.github.io/Implement-Coin-Flipper/",
+    "topics": "HTML, Javascript, CSS"
+    }
+]
